d6f2e75e16cb70c96234283293ccca94b7e8d7d5
HolliverCosta <holliver.costa@ccc.ufcg.edu.br>
2022-09-01 18:33:34 -0300

    test: controller test signup

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -12,5 +12,20 @@ describe("Sign up Controller", () => {
         };
         const httpResponse = sut.handle(httpRequest);
         expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new Error("Missing param: name"));
+    });
+
+    test("Should return 400 if no email is provided", () => { 
+        const sut = new SignUpController();
+        const httpRequest = {
+            body: {
+                name: "teste",
+                password: "testepassword",
+                passwordConfirmation: "testepassword"
+            }
+        };
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new Error("Missing param: email"));
     });
 });
