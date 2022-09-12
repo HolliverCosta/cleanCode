5481f7948e0cde90a52560801ffe91cd16766b8e
HolliverCosta <holliver.costa@ccc.ufcg.edu.br>
2022-09-12 15:55:00 -0300

    test: teste de password confirmation

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -41,4 +41,17 @@ describe("Sign up Controller", () => {
         expect(httpResponse.statusCode).toBe(400);
         expect(httpResponse.body).toEqual(new MissingParamError("password"));
     });
+    test("Should return 400 if no password confirmation is provided", () => { 
+        const sut = new SignUpController();
+        const httpRequest = {
+            body: {
+                name: "teste",
+                email: "teste@gmail.com",
+                password: "testepassword"
+            }
+        };
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new MissingParamError("passwordConfirmation"));
+    });
 });
