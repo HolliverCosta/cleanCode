271e47bd743f2d2103b065f632e845fd0ae75b22
HolliverCosta <holliver.costa@ccc.ufcg.edu.br>
2022-09-14 17:01:25 -0300

    test: erro ao add account

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -167,6 +167,23 @@ describe("Sign up Controller", () => {
         expect(httpResponse.statusCode).toBe(500);
         expect(httpResponse.body).toEqual(new ServerError());
     });
+    test("Should return 500 if AddAccount throws", () => {
+        const { sut , addAccountStub } = makeSut();
+        jest.spyOn(addAccountStub, "add").mockImplementationOnce(() => {
+            throw new Error();
+        });
+        const httpRequest = {
+            body: {
+                name: "teste",
+                email: "invalid@gmail.com",
+                password: "testepassword",
+                passwordConfirmation: "testepassword"
+            }
+        };
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(500);
+        expect(httpResponse.body).toEqual(new ServerError());
+    });
     test("Should call AddAccount with correct values", () => { 
         const { sut, addAccountStub } = makeSut();
 
