ce3e992309a1a29442456ae84ae02ae66352d5c9
HolliverCosta <holliver.costa@ccc.ufcg.edu.br>
2022-09-14 17:21:33 -0300

    test: return 200 com o obj valido

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -17,7 +17,7 @@ const makeAddAccount = (): AddAccount => {
                 id: "valid_id",
                 name: "valid_name",
                 email: "valid_email",
-                password: "valid@mail.com"
+                password: "valid_password"
             };
             return fakeAccount; 
         }
@@ -203,6 +203,26 @@ describe("Sign up Controller", () => {
             name: "any_name",
             email: "any_email@gmail.com",
             password: "any_password",
+        });
+    });
+    test("Should return 200 if an valid data is provided", () => { 
+        const { sut } = makeSut();
+
+        const httpRequest = {
+            body: {
+                name: "valid_name",
+                email: "valid_email",
+                password: "valid_password",
+                passwordConfirmation: "valid_password"
+            }
+        };
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(200);
+        expect(httpResponse.body).toEqual({
+            id: "valid_id",
+            name: "valid_name",
+            email: "valid_email",
+            password: "valid_password",
         });
     });
 });
