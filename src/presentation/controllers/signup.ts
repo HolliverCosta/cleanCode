2a85254c419c885d9307518f55d4899b63cf696d
HolliverCosta <holliver.costa@ccc.ufcg.edu.br>
2022-09-14 15:14:49 -0300

    feat: addAccount com valores corretos

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,12 +1,16 @@
 import { HttpResponse, HttpRequest, Controller, EmailValidator } from "../protocols";
 import { MissingParamError, InvalidParamError } from "../errors";
 import { badRequest, serverError } from "../helper/http-helper";
+import { AddAccount } from "../../domain/usecases/add-account";
 
 export class SignUpController implements Controller{
     private readonly emailValidator: EmailValidator;
+    private readonly addAccount: AddAccount;
 
-    constructor(emailValidator: EmailValidator){
+
+    constructor(emailValidator: EmailValidator, addAccount: AddAccount){
         this.emailValidator = emailValidator;
+        this.addAccount = addAccount;
     }
 
     handle(httpRequest: HttpRequest): HttpResponse{
@@ -18,8 +22,8 @@ export class SignUpController implements Controller{
                     return badRequest(new MissingParamError(field));
                 }
             }
-            const { email, password, passwordConfirmation } = httpRequest.body;
-            
+            const { name, email, password, passwordConfirmation } = httpRequest.body;
+
             if(password !== passwordConfirmation){
                 return badRequest(new InvalidParamError("passwordConfirmation"));
             }
@@ -30,6 +34,11 @@ export class SignUpController implements Controller{
                 return badRequest(new InvalidParamError("email"));
     
             } 
+            this.addAccount.add({
+                name, 
+                email,
+                password
+            });
         } catch (error) {
             return serverError();
         }
