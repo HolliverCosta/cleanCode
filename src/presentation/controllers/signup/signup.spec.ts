70cf7eb45939c4772c26fd8478fd8bff4fc90575
HolliverCosta <holliver.costa@ccc.ufcg.edu.br>
2022-09-14 17:35:09 -0300

    refactor: mudei os metodos para async

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -168,8 +168,8 @@ describe("Sign up Controller", () => {
     });
     test("Should return 500 if AddAccount throws", async () => {
         const { sut , addAccountStub } = makeSut();
-        jest.spyOn(addAccountStub, "add").mockImplementationOnce(() => {
-            throw new Error();
+        jest.spyOn(addAccountStub, "add").mockImplementationOnce(async () => {
+            return new Promise((resolve, reject) => reject (new Error()));
         });
         const httpRequest = {
             body: {
