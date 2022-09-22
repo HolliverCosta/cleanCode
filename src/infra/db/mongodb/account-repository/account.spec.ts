33a6aeb294da1062855c2215cb7cd8b0593c4bd0
HolliverCosta <holliver.costa@ccc.ufcg.edu.br>
2022-09-22 16:09:01 -0300

    refactor: criei o make sut

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -9,8 +9,13 @@ describe("Account mongo repository", () => {
     afterAll(async () => {
         await MongoHelper.disconnect();
     });
+
+    const makeSut = (): AccountMongoRepository => {
+        return new AccountMongoRepository();
+    };
+    
     test("Should return an account on success", async() => {
-        const sut = new AccountMongoRepository();
+        const sut = makeSut();
         const account = await sut.add({
             name: "any_name",
             email: "any@mail.com",
