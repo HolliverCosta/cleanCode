4bc9e5f36ac71e32e34b5b86810456af8bc6f460
HolliverCosta <holliver.costa@ccc.ufcg.edu.br>
2022-09-22 16:07:27 -0300

    feat: adicionei o accountmongoRepository

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,5 +1,4 @@
-import { MongoClient } from "mongodb";
-import { disconnect } from "process";
+import { Collection, MongoClient } from "mongodb";
 
 
 export const MongoHelper = {
@@ -10,5 +9,8 @@ export const MongoHelper = {
     },
     async disconnect (): Promise<void>  {
         await this.client.close();
+    },
+    getCollection (name: string): Collection{
+        return this.client.db().collection(name);
     }
 };
